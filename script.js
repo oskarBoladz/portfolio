838f583c727da633455e86619facd23b5d5a8734
oskarBoladz <103318195+oskarBoladz@users.noreply.github.com>
2023-12-05 14:24:26 +0100

    .4 z

---
+++ script.js
@@ -0,0 +1,6 @@
+ctb=document.getElementById("control")
+ctb.onclick=()=>{
+    // console.log( document.getElementById("lr"))
+    document.getElementById("lr").style.transform="translateY(0%)"
+    document.getElementById("l").style.filter="blur(100px)"
+}
