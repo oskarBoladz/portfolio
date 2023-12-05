2bf654e88d416ab84eac1a623e46a0b5944bc048
oskarBoladz <103318195+oskarBoladz@users.noreply.github.com>
2023-12-05 15:21:29 +0100

    dziala 4.5 ,

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-ctb=document.getElementById("control")
+ctb=document.getElementById("option-work")
 ctb.onclick=()=>{
     // console.log( document.getElementById("lr"))
     document.getElementById("lr").style.transform="translateY(0%)"
     document.getElementById("l").style.filter="blur(100px)"
+}
+
+
+
+ctb=document.getElementById("option-home")
+ctb.onclick=()=>{
+    // console.log( document.getElementById("lr"))
+    document.getElementById("lr").style.transform="translateY(-120%)"
+    document.getElementById("l").style.filter="blur(0px)"
 }
