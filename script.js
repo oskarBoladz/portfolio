0280c551367752a11492ccbe7785e67fb098617b
oskarBoladz <103318195+oskarBoladz@users.noreply.github.com>
2023-12-09 17:42:41 +0100

    , ani wor

---
+++ script.js
@@ -0,0 +1,24 @@
+ctb=document.getElementById("option-work")
+document.getElementById("l2").style.transform="translateY(-100svh)"
+ctb.onclick=()=>{
+    // console.log( document.getElementById("lr"))
+    document.getElementById("l2").style.transform="translateY(0svh)"
+    document.getElementById("l").style.filter="blur(100px)"
+    document.getElementById("cf").style.gridRow="1/5"
+    document.getElementById("cf").style.filter="blur(100px)"
+    document.getElementById("sb").style.filter="blur(100px)"
+    document.getElementById("rwp").style.transform="translateX(0px)"
+}
+
+
+
+ctb=document.getElementById("option-home")
+ctb.onclick=()=>{
+    // console.log( document.getElementById("lr"))
+    document.getElementById("l2").style.transform="translateY(-100svh)"
+    document.getElementById("l").style.filter="blur(0px)"
+    document.getElementById("cf").style.gridRow="1/4"
+    document.getElementById("rwp").style.transform="translateX(50vw)"
+    document.getElementById("cf").style.filter="blur(0px)"
+    document.getElementById("sb").style.filter="blur(0px)"
+}
