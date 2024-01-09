f19383fdc4a08d1d7c45246acdc4ee769b4d220d
oskarBoladz <103318195+oskarBoladz@users.noreply.github.com>
2024-01-09 14:45:10 +0100

    j

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ ctb.onclick=()=>{
     // console.log( document.getElementById("lr"))
     document.getElementById("l2").style.transform="translateY(0svh)"
     document.getElementById("l").style.filter="blur(100px)"
-    document.getElementById("cf").style.gridRow="1/5"
+    // document.getElementById("cf").style.gridRow="1/5"
     document.getElementById("cf").style.filter="blur(100px)"
     document.getElementById("sb").style.filter="blur(100px)"
     document.getElementById("rwp").style.transform="translateX(0px)"
@@ -17,7 +17,7 @@ ctb.onclick=()=>{
     // console.log( document.getElementById("lr"))
     document.getElementById("l2").style.transform="translateY(-100svh)"
     document.getElementById("l").style.filter="blur(0px)"
-    document.getElementById("cf").style.gridRow="1/4"
+    // document.getElementById("cf").style.gridRow="1/4"
     document.getElementById("rwp").style.transform="translateX(50vw)"
     document.getElementById("cf").style.filter="blur(0px)"
     document.getElementById("sb").style.filter="blur(0px)"
